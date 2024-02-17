fda172addfd5cf5d2f8ebd0b73a4ffea4eccd341
kaesly26 <k4t70246@gmail.com>
2024-02-17 10:09:53 -0500

    se creo el formulario de riesgos, causas y dependencia

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,131 +1,109 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
+  <v-card class="card">
+    <v-form class="form">
+      <v-col cols="11">
+        <h1 class="titulo">RIESGOS</h1>
+        <v-text-field
+          label="Nombre del riesgo"
+          v-model="datosRiesgo.nombre"
+        ></v-text-field>
+        <v-select label="Dependencia" :items="items"></v-select>
+        <textarea
+          class="textarea"
+          placeholder="Descripcion"
+          v-model="datosRiesgo.descripcion"
+        ></textarea>
+        <v-btn class="btn">Guardar</v-btn>
       </v-col>
+    </v-form>
+  </v-card>
 
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to the Vuetify 3 Beta
-        </h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
-            >Discord Community</a
-          >
-        </p>
+  <v-card class="card">
+    <v-form class="form">
+      <v-col cols="11">
+        <h1 class="titulo">CAUSA</h1>
+        <v-text-field
+          label="nombre causa"
+          v-model="datosCausa.nombre"
+        ></v-text-field>
+        <v-select label="Riesgo" :items="riesgos"></v-select>
+        <textarea
+          class="textarea"
+          placeholder="Descripcion"
+          v-model="datosCausa.descripcion"
+        ></textarea>
+        <v-btn class="btn">Guardar</v-btn>
       </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-5">What's next?</h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
+    </v-form>
+  </v-card>
+  <v-card class="card">
+    <v-form class="form">
+      <v-col cols="11">
+        <h1 class="titulo">DEPENDENCIAS</h1>
+        <v-text-field label="nombre dependencia"></v-text-field>
+        <v-btn class="btn">Guardar</v-btn>
       </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-5">Important Links</h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-5">Ecosystem</h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+    </v-form>
+  </v-card>
 </template>
-
+<style>
+.card {
+  text-align: center;
+  width: 500px;
+  height: 600px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #c7c8c8;
+}
+.form {
+  width: 500px;
+  height: 600px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.textarea {
+  width: 440px;
+  height: 200px;
+}
+.btn {
+  top: 30px;
+}
+</style>
 <script>
+import axios from 'axios';
+
 export default {
   name: "HelloWorld",
 
   data: () => ({
-    ecosystem: [
-      {
-        text: "vuetify-loader",
-        href: "https://github.com/vuetifyjs/vuetify-loader/tree/next",
-      },
-      {
-        text: "github",
-        href: "https://github.com/vuetifyjs/vuetify/tree/next",
-      },
-      {
-        text: "awesome-vuetify",
-        href: "https://github.com/vuetifyjs/awesome-vuetify",
-      },
-    ],
-    importantLinks: [
-      {
-        text: "Chat",
-        href: "https://community.vuetifyjs.com",
-      },
-      {
-        text: "Made with Vuetify",
-        href: "https://madewithvuejs.com/vuetify",
-      },
-      {
-        text: "Twitter",
-        href: "https://twitter.com/vuetifyjs",
-      },
-      {
-        text: "Articles",
-        href: "https://medium.com/vuetify",
-      },
-    ],
-    whatsNext: [
-      {
-        text: "Explore components",
-        href: "https://vuetifyjs.com",
-      },
-      {
-        text: "Roadmap",
-        href: "https://vuetifyjs.com/introduction/roadmap/",
-      },
-      {
-        text: "Frequently Asked Questions",
-        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
-      },
-    ],
+    select: null,
+    items: ["Coordinador de formacion", "Coordinador academico", "Lider ficha"],
+    riesgos: ["riesgos"],
+
+    datosRiesgo: {
+      nombre: "",
+      descripcion: "",
+      dependencia: "",
+    },
+    datosCausa: {
+      nombre: "",
+      descripcion: "",
+      riesgo: "",
+    },
   }),
+  mounted() {
+    axios.get("http://localhost:3000/Riesgo").then(response =>{
+      console.log(response.data),
+    }).catch(error =>{
+      console.log(error)
+    })
+
+  }
 };
 </script>
